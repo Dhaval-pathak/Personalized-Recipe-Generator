71e49441e382de8f62704f2e26f3c2ce16130525
Dhaval pathak <dhavalpathak14@gmail.com>
2025-06-11 15:40:50 +0530

    enhancements: docker compose file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-console.log(process.env.REACT_APP_API_BASE_URL);
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+console.log(process.env.REACT_APP_API_URL);
 
 const login = async (email, password) => {
     const response = await axios.post(`${API_BASE_URL}/login`, { email, password });
